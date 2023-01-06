6c9fd01efccba537c82cb819b03f3eea56882b67
Rafael Noll <rafanoll2004@gmail.com>
2023-01-06 17:14:47 -0300

    Changing the baseUrl of the api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:3030",
+	baseURL: "https://react-music-api-production.up.railway.app",
 });
 
 export default api;
